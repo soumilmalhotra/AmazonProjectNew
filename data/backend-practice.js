454e7ae8db7a2c9e474ca4744bf6673073c42a50
soumilmalhotra <malhotrasoumil1@gmail.com>
2025-01-17 01:01:00 +0530

    minor changes

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -6,5 +6,3 @@ xhr.addEventListener('load', ()=> {
 
 xhr.open('GET', 'https://supersimplebackend.dev/images/apple.jpg');
 xhr.send();
-
-asdfkljfasdjkfjkasdjfkasdjkfjasdkfjasdjfkasd
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../backend-practice.js';
+import '../data/backend-practice.js';
 renderOrderSummary();
 renderPaymentSummary();

0820dc0582f5a2878ba9c3a241a2b2cb28b3edb0
soumilmalhotra <malhotrasoumil1@gmail.com>
2025-01-19 01:52:05 +0530

    made the order and return page generate its own html by itslef

--- data/order-display.js
+++ data/order-display.js
@@ -1,86 +1,102 @@
 import { orders } from "./orders.js";
-import { products } from "./products.js";
+import { products, loadProductsFetch } from "./products.js";
+loadPage();
+async function loadPage() {
+    await loadProductsFetch();
+    loadOrders();
+}
+
+function loadOrders(){
 console.log(orders);
 orders.forEach(order => {
+    const dateObject = new Date(order.orderTime);
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ];
+    const month = monthNames[dateObject.getMonth()];
     
-    document.querySelector('.orders-grid').innerHTML += `
-     <div class="order-container">
-          
-          <div class="order-header">
-            <div class="order-header-left-section">
-              <div class="order-date">
-                <div class="order-header-label">Order Placed:</div>
-                <div>August 12</div>
-              </div>
-              <div class="order-total">
-                <div class="order-header-label">Total:</div>
-                <div>$${(order.totalCostCents/100).toFixed(2)}</div>
-              </div>
-            </div>
+    const date = dateObject.getDate();
+    
+    console.log();
 
+    let html = '';
+    html+= `
+    <div class="order-container">
+    
+    <div class="order-header">
+    <div class="order-header-left-section">
+    <div class="order-date">
+                <div class="order-header-label">Order Placed:</div>
+                <div>${month} ${date}</div>
+                </div>
+                    <div class="order-total">
+                        <div class="order-header-label">Total:</div>
+                        <div>$${(order.totalCostCents/100).toFixed(2)}</div>
+                    </div>
+                </div>
+                
             <div class="order-header-right-section">
-              <div class="order-header-label">Order ID:</div>
+            <div class="order-header-label">Order ID:</div>
               <div>${order.id}</div>
             </div>
           </div>
+          
 
-          <div class="order-details-grid">
-            <div class="product-image-container">
-              <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
-            </div>
+          `
+          order.products.forEach((product) =>{
+            let productToBeDisplayed;
+            products.forEach(productOg => {
+                if (productOg.id === product.productId) {
+                    const estimatedDeliveryDateObject = new Date(product.estimatedDeliveryTime);
+                    const estimatedDeliveryMonth = monthNames[estimatedDeliveryDateObject.getMonth()];
+                    const estimatedDeliveryDate = estimatedDeliveryDateObject.getDate();
+                    const formatedEstimatedDeliveryDate = `${estimatedDeliveryMonth} ${estimatedDeliveryDate}`
+                    html+=
+                    `
+                    <div class="order-details-grid">
+                      <div class="product-image-container">
+                        <img src="${productOg.image}">
+                      </div>
+          
+                      <div class="product-details">
+                        <div class="product-name">
+                          ${productOg.name}
+                          </div>
+                        <div class="product-delivery-date">
+                          Arriving on: ${formatedEstimatedDeliveryDate}
+                        </div>
+                        <div class="product-quantity">
+                        Quantity: ${product.quantity}
+                        </div>
+                        <button class="buy-again-button button-primary">
+                        <img class="buy-again-icon" src="images/icons/buy-again.png">
+                        <span class="buy-again-message">Buy it again</span>
+                        </button>
+                        </div>
+                        
+                        <div class="product-actions">
+                        <a href="tracking.html">
+                        <button class="track-package-button button-secondary">
+                            Track package
+                          </button>
+                        </a>
+                      </div>
+                      </div>
+                      `
+                } 
+            });
 
-            <div class="product-details">
-              <div class="product-name">
-                Black and Gray Athletic Cotton Socks - 6 Pairs
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 15
-              </div>
-              <div class="product-quantity">
-                Quantity: 1
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
+           
+          })
 
-            <div class="product-actions">
-              <a href="tracking.html">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
 
-            <div class="product-image-container">
-              <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
-            </div>
+   html += `
 
-            <div class="product-details">
-              <div class="product-name">
-                Adults Plain Cotton T-Shirt - 2 Pack
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 19
-              </div>
-              <div class="product-quantity">
-                Quantity: 2
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-    `
+   
+   </div>
+   `
+   
+   document.querySelector('.orders-grid').innerHTML += html;
 });
+}
